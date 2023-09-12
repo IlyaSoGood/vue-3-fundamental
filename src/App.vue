ad3924a616a3f7013f21726e7f25bc458baa4ab6
Ilya Ermolin <ermolin.ea@filo-soft.ru>
2023-09-12 09:41:51 +0300

    Сортировка через computed (без изменения массива posts в data)

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
     </my-dialog>
 
     <post-list
-        :posts="posts"
+        :posts="sortedPosts"
         @remove="removePost"
         v-if="!isPostLoading"
     />
@@ -75,10 +75,11 @@ export default {
     this.fetchPosts();
   },
   watch: {
-    selectedSort(newValue) {
-      this.posts.sort((post1, post2) => {
-        return post1[newValue]?.localeCompare(post2[newValue])
-      })
+
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
     }
   }
 }

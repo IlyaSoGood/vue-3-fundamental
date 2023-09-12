a2f25d51bb7411e06b351bb6b3e2c9a25d355869
Ilya Ermolin <ermolin.ea@filo-soft.ru>
2023-09-12 10:21:20 +0300

    Анимация transition-group, +move transition

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,16 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
-<!--    Передаем 'remove' еще выше-->
-    <post-item
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+    <!--Передаем 'remove' еще выше-->
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
+
   </div>
   <h2 v-else style="color: red">
     Список постов пуст
@@ -30,5 +33,16 @@ export default {
 </script>
 
 <style scoped>
-
+  .post-list-enter-active,
+  .post-list-leave-active {
+    transition: all 0.4s ease;
+  }
+  .post-list-enter-from,
+  .post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+  .post-list-move {
+    transition: transform 0.4s ease;
+  }
 </style>
